dc2f57837bc0523483ff12fd00197be6bade17e5
Samuel Molina <samuelmolina281816@gmail.com>
2024-11-01 16:02:36 -0500

    adding documentation for transaction of reservations

--- src/main/java/com/example/backend/reservas/application/TransaccionCommandServiceImp.java
+++ src/main/java/com/example/backend/reservas/application/TransaccionCommandServiceImp.java
@@ -10,12 +10,26 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+/**
+ * Service implementation for handling commands related to Transacciones.
+ */
 @Service
 public class TransaccionCommandServiceImp implements TransaccionCommandService {
     private final TransaccionRepository transaccionRepository;
 
+    /**
+     * Constructs a new TransaccionCommandServiceImp with the specified TransaccionRepository.
+     *
+     * @param transaccionRepository the repository to be used for accessing Transaccion data
+     */
     public TransaccionCommandServiceImp(TransaccionRepository transaccionRepository) { this.transaccionRepository = transaccionRepository;}
 
+    /**
+     * Handles the CreateTransaccionCommand to create a new Transaccion.
+     *
+     * @param command the command containing the details of the Transaccion to be created
+     * @return an Optional containing the created Transaccion if successful, or an empty Optional if not
+     */
     @Override
     public Optional<Transaccion> handle(CreateTransaccionCommand command) {
         var datosTransaccion = new Transaccion(command.datosTransaccion());
